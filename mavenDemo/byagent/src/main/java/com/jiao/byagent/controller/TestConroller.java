cded8191c8fac37f988ee85c83dc9cde0bae3407
jiao <jiaojianjun1991@gmail.com>
2018-10-20 23:09:56 +0800

    引用了thymeleaf和热部署

---
+++ mavenDemo/byagent/src/main/java/com/jiao/byagent/controller/TestConroller.java
@@ -0,0 +1,20 @@
+package com.jiao.byagent.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Jiao on 2018/10/20.
+ */
+@Controller
+public class TestConroller {
+
+    @RequestMapping(value = "index", method = RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("name", "知识林-jiao-jiao");
+        return "hello";
+    }
+
+}
