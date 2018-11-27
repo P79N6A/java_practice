f15cb7c4262149c6c45e7e20040978bb9277b99b
isjianjun <is.jianjun@gmail.com>
2018-11-27 17:09:40 +0800

    2018-11-27

--- mall/e3searchweb/src/main/java/com/jiao/searchweb/SearchWeb.java
+++ mall/e3searchweb/src/main/java/com/jiao/searchweb/SearchWeb.java
@@ -3,6 +3,7 @@ package com.jiao.searchweb;
 import com.jiao.common.pojo.SearchResult;
 import com.jiao.research.SearchItems;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
